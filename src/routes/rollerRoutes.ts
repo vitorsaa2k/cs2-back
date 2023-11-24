e0fe32b302a67209802ffbf77f77fcbd29854853
JookDev <123021263+JookDev@users.noreply.github.com>
2023-11-24 17:50:16 -0300

    Delete src/routes/rollerRoutes.ts
    the roller route has no meaning to exists, since is a subroute of crate route

--- src/routes/rollerRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleOpenCreate } from "../controller/crateController";
-
-const rollerRoutes = Router();
-
-rollerRoutes.get("/:name", handleOpenCreate);
-
-export { rollerRoutes };
