d44abd1e350908357fe90fe2e4676b0622128002
vitoreow <vitoradriel66@gmail.com>
2023-11-25 18:39:45 -0300

    fix prod url

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
-export const FRONT_URL = process.env.NODE_ENV
-	? "https://skinsmania.vercel.app"
-	: "http://localhost:5173";
+export const FRONT_URL =
+	process.env.NODE_ENV === "production"
+		? "https://skinsmania.vercel.app"
+		: "http://localhost:5173";
