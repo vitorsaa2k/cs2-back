a53b08be994edf93f35ce9bb70396f6aa49a1273
vitoreow <vitoradriel66@gmail.com>
2023-12-17 14:24:42 -0300

    auto emit to all sockets on connection

--- src/socketHelpers/handleOnlineUsers.ts
+++ src/socketHelpers/handleOnlineUsers.ts
@@ -1,6 +1,6 @@
 import { Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
-
+import io from "../config/socket";
 let users: { [key: string]: string | undefined } = {};
 
 function handleOnlineUsers(
@@ -10,7 +10,7 @@ function handleOnlineUsers(
 	if (userId) {
 		users[`${userId}`] = userId;
 	}
-	socket.emit("usercount", Object.keys(users));
+	io().emit("usercount", Object.keys(users));
 	socket.on("disconnect", () => {
 		const userId = socket.request.session.passport?.user.id;
 		if (userId) {
