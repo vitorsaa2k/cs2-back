dc2a92e56ffac598383fc05608cf81f404c6037b
vitoreow <vitoradriel66@gmail.com>
2024-01-27 15:02:29 -0300

    create addSkinsToInventory

---
+++ src/helpers/addSkinsToInventory.ts
@@ -0,0 +1,13 @@
+import { Inventory } from "../models/InventoryModel";
+import { DrawnSkin } from "../types/crateTypes";
+
+async function addSkinsToInventory(skins: DrawnSkin[], userId: string) {
+	const inventory = await Inventory.findOne({ id: userId });
+	skins.forEach(skin => {
+		inventory?.inventory.push(skin);
+	});
+	if (inventory) await inventory.save();
+	return inventory;
+}
+
+export { addSkinsToInventory };
