e05d858792ecfd2d69e61387cb850a8c7e01ef4e
vitorsaa2k <vitoradriel66@gmail.com>
2025-12-11 17:00:20 -0300

    add filters to getSkinRange controller

--- src/controller/skinController.ts
+++ src/controller/skinController.ts
@@ -1,6 +1,6 @@
 import { Skin } from "../models/SkinModel";
 import { Request, Response } from "express";
-import { filterArrayForPage } from "../utils/filterArrayForPage";
+import { RarityNames, SortOptions } from "../types/types";
 
 const addSkin = async (req: Request, res: Response) => {
 	const newSkin = new Skin(req.body);
@@ -22,28 +22,94 @@ const removeAllSkins = async (req: Request, res: Response) => {
 
 const getSkinByRange = async (req: Request, res: Response) => {
 	const maxPrice = parseInt((req.query.maxPrice as string) ?? "0");
-	const page = req.query.page;
+	const page = parseInt((req.query.page as string) ?? "1");
+	const sort = req.query.sort as SortOptions;
+	const name = req.query.name as string;
+	const rarity = req.query.rarity as RarityNames;
+
 	const itemsPerPage = 15;
-	const pageNumber = parseInt((page as string) ?? "1");
+	let maxPages = 0;
+	const itemsToSkip = (page - 1) * itemsPerPage;
+
 	if (maxPrice === 0) {
 		try {
-			const skin = await Skin.aggregate([
-				{ $sort: { price: -1 } },
-				{ $limit: itemsPerPage * pageNumber },
+			const skinsQueryPromise = Skin.aggregate([
+				{
+					$match: {
+						name: { $regex: name, $options: "i" },
+						rarity: { $regex: rarity, $options: "i" },
+					},
+				},
+				{ $sort: { price: sort === "DESC" ? 1 : -1 } },
+				{ $skip: itemsToSkip },
+				{ $limit: itemsPerPage },
 			]);
-			const skinsToSend = filterArrayForPage(skin, pageNumber, itemsPerPage);
-			if (skin) return res.status(200).json(skinsToSend);
+
+			const totalSkinsPromise = Skin.countDocuments({
+				name: { $regex: name, $options: "i" },
+				rarity: { $regex: rarity, $options: "i" },
+			});
+
+			const [skin, total] = await Promise.all([
+				skinsQueryPromise,
+				totalSkinsPromise,
+			]);
+
+			maxPages = Math.ceil(total / itemsPerPage);
+			if (skin)
+				return res.status(200).json({
+					skins: skin,
+					pagination: {
+						page: page,
+						itemsPerPage,
+						totalItems: skin.length,
+						maxPages: maxPages,
+					},
+				});
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
 	if (maxPrice > 0) {
-		const skin = await Skin.find({ price: { $lte: Number(maxPrice) } })
-			.limit(itemsPerPage * pageNumber)
-			.sort({ price: -1 });
-		const skinsToSend = filterArrayForPage(skin, pageNumber, itemsPerPage);
-		if (skin) return res.status(200).json(skinsToSend);
+		try {
+			const skinsQueryPromise = Skin.aggregate([
+				{
+					$match: {
+						price: { $lte: Number(maxPrice) },
+						name: { $regex: name, $options: "i" },
+						rarity: { $regex: rarity, $options: "i" },
+					},
+				},
+				{ $sort: { price: sort === "DESC" ? 1 : -1 } },
+				{ $skip: itemsToSkip },
+				{ $limit: itemsPerPage },
+			]);
+
+			const totalSkinsPromise = Skin.countDocuments({
+				price: { $lte: Number(maxPrice) },
+				name: { $regex: name, $options: "i" },
+				rarity: { $regex: rarity, $options: "i" },
+			});
+
+			const [skin, total] = await Promise.all([
+				skinsQueryPromise,
+				totalSkinsPromise,
+			]);
+			maxPages = Math.ceil(total / itemsPerPage);
+			if (skin)
+				return res.status(200).json({
+					skins: skin,
+					pagination: {
+						page: page,
+						itemsPerPage,
+						totalItems: skin.length,
+						maxPages: maxPages,
+					},
+				});
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	return res.status(400).json({ error: { message: "There was an error" } });
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export type SortOptions = "ASC" | "DESC";
+
+export type RarityNames =
+	| "Consumer Grade"
+	| "Industrial Grade"
+	| "Mil-Spec Grade"
+	| "Restricted"
+	| "Classified"
+	| "Covert"
+	| "Contraband";
